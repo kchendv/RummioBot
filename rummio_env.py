3c83c41eec96898e5a230e4313a0e27b8ccaa0d5
Kevin Chen <kchendv@gmail.com>
2025-07-19 19:28:29 -0700

    Complete rummio env

--- rummio_env.py
+++ rummio_env.py
@@ -4,23 +4,34 @@ from gymnasium.envs.registration import register
 from gymnasium.utils.env_checker import check_env
 import numpy as np
 
+from rummio_game import RummioGame
+
+
+register(
+    id = "rummio",
+    entry_point="rummio_env:RummioEnv",
+    nondeterministic=True
+)
+
 class RummioEnv(gym.Env):
     metadata = {"render_modes":["human"], "render_fps":1}
 
     def __init__(self, render_mode = None):
+        self.game = RummioGame()
+        self.prev_state = None
         self.render_mode = render_mode
         self.action_space = spaces.Discrete(25) # Size of grid
         self.observation_space = spaces.Box(
             low = 0,
-            high = np.array([11] for i in range(26)),
+            high = np.array([11 for i in range(26)]),
             shape=(26,),
             dtype= np.int32
         )
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
-
-        obs = [] # TODO ADD
+        self.game.reset()
+        obs = self.game.get_state()
         info = {}
         if(self.render_mode=='human'):
             self.render()
@@ -28,14 +39,26 @@ class RummioEnv(gym.Env):
         return obs, info
     
     def step(self, action):
-        # TODO TAKE ACTION
-        is_game_end = 1 # TODO ADD
-        info = {}
-        obs = {} # TODO ADD
-        return obs, (0 if is_game_end else 1), is_game_end, False, info
+        self.game.do_click(action)
+        obs = self.game.get_state()
+        is_game_end = self.game.is_game_end(obs)
+        reward = 0
+        if (is_game_end):
+            reward = -1
+        elif obs == self.prev_state:
+            reward = -1
+        else:
+            reward = 1
+        self.prev_state = obs
+        info = {
+            "Action": action,
+            "Reward": reward,
+            "Is end": is_game_end
+        }
 
+        return obs, reward, is_game_end, False, info
 
-    def render():
+    def render(self):
         pass
 
 
@@ -46,4 +69,5 @@ if __name__ == "__main__":
     obs = env.reset()[0]
     for i in range(10):
         rand_action = env.action_space.sample()
-        obs, reward, terminated, _, _ = env.step(rand_action)
+        obs, reward, terminated, _, info = env.step(rand_action)
+        print(obs, info)
---
+++ rummio_game.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+class RummioGame():
+    def __init__(self):
+        # Setup Chrome options
+        options = webdriver.ChromeOptions()
+        options.add_argument("--start-maximized")  # Optional: Start with a maximized window
+
+        # Create WebDriver (Make sure chromedriver is in your PATH or specify the full path)
+        self.driver = webdriver.Chrome(service=Service(), options=options)
+
+        # Load the page
+        self.driver.get("https://rumm.io")
+
+        # Wait for page to load content
+        time.sleep(3)  # Increase if needed
+
+
+        # The browser stays open
+
+    def get_state(self):
+        # Get all cell-top divs inside the main 5x5 grid (not tutorial or win-grid)
+        grid_cells = self.driver.find_elements(By.XPATH, '//div[@class="grid"]//div[@class="row"]//div//div')
+        # Extract the class name (e.g., c0, c1...) and parse the numeric value
+        values = []
+        for cell in grid_cells:
+            class_attr = cell.get_attribute("class")
+            match = class_attr.split(" ")
+            values.append(int(match[-1][1:]))
+
+
+        input_cell = self.driver.find_elements(By.XPATH, '//div[@id="next"]//div')
+        class_attr = input_cell[0].get_attribute("class")
+        match = class_attr.split(" ")
+        values.append(int(match[-1][1:]))
+        return values
+    
+    def is_game_end(self, values):
+        return not(0 in values)
+    
+    def do_click(self, index):
+        grid_cells = self.driver.find_elements(By.XPATH, '//div[@class="grid"]//div[@class="row"]//div//div')
+        grid_cells[index].click()
+
+    def reset(self):
+        reset_button = self.driver.find_element(By.XPATH, '//div[text()="restart"]')
+        reset_button.click()
